98c237af468693a161346bcb32df53c0b834f3d3
julesow <souleymanesow@gmail.com>
2018-12-12 10:30:28 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+    def  name=(name)
+        # writes the name of the person to an instance variable @name (FAILED - 2)
+        @name = name
+    end
+    def  name
+        # reads the name of the person from an instance variable @name (FAILED - 3)
+        @name
+
+    end
+    def  job=(job)
+        # writes the job of the person to an instance variable @job (FAILED - 4)
+      @job=job
+
+    end
+    def  job
+      @job
+    end
+end # reads the job of the person from an instance variable @job (FAILED - 5)end
